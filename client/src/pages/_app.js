31278584b10c9795b307ac9bbaacd192cc5a926f
erubae <115371540+ekedayen-e@users.noreply.github.com>
2023-07-24 10:31:56 +0100

    done

--- client/src/pages/_app.js
+++ client/src/pages/_app.js
@@ -3,11 +3,16 @@ import { AuthProvider } from '@/context/AuthProvider';
 import { ThemeProvider } from '@/context/ThemeProvider'
 import '@/styles/globals.css'
 import {useState} from 'react'
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
   const [search, setSearch] = useState('');
   return (
   <ThemeProvider>
+    <Head>
+    <title>Romanga</title>
+    <link rel="icon" href="/favicon.ico"/>
+    </Head>
     <div id="portal"></div>
     <AuthProvider>
     <Layout setSearch={setSearch}>
